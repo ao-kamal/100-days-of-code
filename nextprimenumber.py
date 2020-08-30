3199fff42211878ae1b33fc4914f25664e184908
Kamal Alagbo <alagbokamal01@gmail.com>
2020-08-30 10:37:41 +0100

    Initial commit

---
+++ nextprimenumber.py
@@ -0,0 +1,15 @@
+def print_prime():
+	count = 0
+	number = 0
+	if number
+def start_prompt():
+	user_input = input('Type Start to begin')
+	if prompt == 'Start':
+		print_prime()
+	else:
+		print('You typed {0}, dummy :-('.format(prompt))
+		start_prompt()
+
+print('Prime Number Finder: This Program will find a prime number until the user chooses to stop asking for the next one.')
+
+start_prompt()
---
+++ pi.py
@@ -0,0 +1,4 @@
+import math
+import cmath
+print(math.pi)
+print(cmath.pi)
