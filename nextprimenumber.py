e2e57f1f908098d17f07df855f76d279a216051d
Kamal Alagbo <alagbokamal01@gmail.com>
2020-09-01 23:45:40 +0100

    .

--- nextprimenumber.py
+++ nextprimenumber.py
@@ -1,15 +1,30 @@
-def print_prime():
-	count = 0
-	number = 0
-	if number
-def start_prompt():
-	user_input = input('Type Start to begin')
-	if prompt == 'Start':
-		print_prime()
-	else:
-		print('You typed {0}, dummy :-('.format(prompt))
-		start_prompt()
+import math
 
 print('Prime Number Finder: This Program will find a prime number until the user chooses to stop asking for the next one.')
+choice = input('First number? (Type Y or N)\n')
+current = 1
 
-start_prompt()
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
+
+    max_div = math.floor(math.sqrt(n))
+    for i in range(3, 1 + max_div, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+while choice.lower().startswith('y'):
+    current += 1
+
+    while is_prime(current) == False:
+        current += 1
+
+    print("Next prime is " + str(current))
+    choice = input("Continue finding prime numbers (y/n)? ")
