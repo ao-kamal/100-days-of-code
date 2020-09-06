2e85d20c78c6d009ec0fab4483066cbcc81a1a7b
Kamal Alagbo <alagbokamal01@gmail.com>
2020-09-06 15:46:21 +0100

    Update creditcardvalidator.py

--- creditcardvalidator.py
+++ creditcardvalidator.py
@@ -2,17 +2,19 @@
 validates it to make sure that it is a valid number (look into how credit cards use a checksum).
 '''
 
-print('enter CC number')
+print('Enter CC number')
 cc = input()
-checkNumber = str(cc[-1:])
+checksum = str(cc[-1:])
 rest = cc[:-1]
 print('Validating...')
-numbers = list(rest)
-toDouble = [0, 2, 4, 6, 8, 10, 12, 14]
-for x in toDouble:
-    n = int(numbers[x])
-    n = int(n) * 2
-    numbers[x] = n
+
+numbers = []
+for i in rest:
+    numbers.append(int(i))
+to_double = [0, 2, 4, 6, 8, 10, 12, 14]
+for x in to_double:
+    numbers[x] *= 2
+
 i = 0
 while i <= 14:
     if numbers[i] >= 10:
@@ -20,10 +22,10 @@ while i <= 14:
         (numbers[i]) = int(numbers[i])
         numbers[i] = sum(map(int, str(numbers[i])))
     i += 1
-sumOf = (sum(numbers))
-sumOf = int(sumOf) * 9
-checkDigit = str(sumOf)[-1:]
-if checkNumber == checkDigit:
+sum_of = (sum(numbers))
+sum_of = int(sum_of) * 9
+check_digit = str(sum_of)[-1:]
+if checksum == check_digit:
     print("It's valid!")
 else:
     print("It's not valid.")
